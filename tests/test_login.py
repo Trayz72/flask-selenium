641bd1d73ad1bb500d3870727bfcec3186831bcb
Trayz72 <shahtanishq72@gmail.com>
2025-09-15 05:11:45 +0530

    update

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,25 +11,25 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")  
 
 service = Service(ChromeDriverManager().install())
-time.sleep(2)
+
 driver = webdriver.Chrome(service=service, options=options)
-time.sleep(2)
+
 
 driver.get("http://127.0.0.1:5050")
-time.sleep(2)
+
 
 wait = WebDriverWait(driver, 10)
 username_field = wait.until(EC.presence_of_element_located((By.NAME, "username")))
 password_field = driver.find_element(By.NAME, "password")
 
-time.sleep(2)
+
 username_field.send_keys("admin")
-time.sleep(2)
+
 password_field.send_keys("secret")
-time.sleep(2)
+
 password_field.send_keys(Keys.RETURN)
 
-time.sleep(10)
+
 
 body_text = driver.find_element(By.TAG_NAME, "body").text
 assert "Login successful!" in body_text
